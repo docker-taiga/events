36ac3c61e41cf4b497bde09c3abe10d409124b2f
w1ck3dg0ph3r <w1ck3dg0ph3r@gmail.com>
2020-12-01 18:27:54 +0300

    Escape secrets before substitution in config

--- start.sh
+++ start.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
+TAIGA_SECRET_ESCAPED=$(echo "$TAIGA_SECRET" | sed 's/[&/\]/\\&/g')
+RABBIT_PASSWORD_ESCAPED=$(echo "$RABBIT_PASSWORD" | sed 's/[&/\]/\\&/g')
+
 sed -e 's/$RABBIT_USER/'$RABBIT_USER'/' \
-	-e 's/$RABBIT_PASSWORD/'$RABBIT_PASSWORD'/' \
+	-e 's/$RABBIT_PASSWORD/'$RABBIT_PASSWORD_ESCAPED'/' \
 	-e 's/$RABBIT_HOST/'$RABBIT_HOST'/' \
 	-e 's/$RABBIT_PORT/'$RABBIT_PORT'/' \
 	-e 's/$RABBIT_VHOST/'$RABBIT_VHOST'/' \
-	-e 's/$TAIGA_SECRET/'$TAIGA_SECRET'/' \
+	-e 's/$TAIGA_SECRET/'$TAIGA_SECRET_ESCAPED'/' \
 	-i config.json
 
 exec coffee index.coffee > /dev/stdout 2> /dev/stderr &
